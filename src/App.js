c85e818c601317a350c143946155797ebc591ebb
milo.masonicic <ilom3774@gmail.com>
2023-11-14 11:36:41 +0100

    fp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+
+let nextId = 0
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  return(
+    <div>
+      <h1>Inspiring sculptors:</h1>
+
+      <input
+        value={todo}
+        onChange={e => setTodo(e.target.value)}
+      />
+        <button onClick={() => {
+          setTodos((prevTodos) => 
+            [...prevTodos, 
+            {
+              id: nextId++,
+              task: todo,
+              completed: false,
+            }
+            ])
+       
+      }}>Add</button>
+
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>id:{todo.id}  task:{todo.task}  completed:{String(todo.completed)}
+          <span> 
+
+            <button onClick={() => {
+              setTodos(
+                todos.filter(t =>
+                  t.id !== todo.id
+                )
+              );
+            }} > Delete</button>
+
+            <input type='checkbox' onChange={() => {
+            setTodos((prevTodos) => prevTodos.map((t) =>
+            t.id === todo.id ? {...t, completed: !t.completed} : t
+            ))
+          }}></input>
+
+          </span>
+          </li>
+        ))}
+      </ul>
+
+
     </div>
-  );
+  )
+  
 }
 
 export default App;
